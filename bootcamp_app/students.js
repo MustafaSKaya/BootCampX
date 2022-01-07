2f0426b52602db6be569981e9b40fa3850cb04d4
MustafaSKaya <mustafakya88@hotmail.com>
2022-01-07 01:51:27 +0000

    queries parameterized

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,15 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, (assistance_requests.completed_at-assistance_requests.started_at) as duration 
-FROM assistance_requests 
-JOIN students ON student_id = students.id 
-JOIN teachers ON teacher_id = teachers.id 
-JOIN assignments ON assignment_id = assignments.id 
-ORDER BY duration;
-`)
-.then(res => {
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
+  .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
